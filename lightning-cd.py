07fba4f7b0af2ba707492dbc6c2534c8f87e4a08
Brendan Le <le.brendan@gmail.com>
2015-10-12 11:43:06 -0700

    Change delete to delete key

--- lightning-cd.py
+++ lightning-cd.py
@@ -185,8 +185,8 @@ if __name__ == '__main__':
                 if letter:
                     if letter in charRange:
                         searchBuffer = searchBuffer + letter
-                    elif letter == keybindings.KEY_DELETE:
-                        searchBuffer = searchBuffer[:-1]
+                elif keycode == 127: # DELETE
+                    searchBuffer = searchBuffer[:-1]
         t.close()
         print os.path.realpath('.')
     except Exception, e:
