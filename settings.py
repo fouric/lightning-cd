48e4e6c509cc08c0030aac8fdc7702c7f0c3ec27
Grant Vesely <fouric@users.noreply.github.com>
2015-10-28 20:37:34 -0700

    Updated documentation; added option to restrict characters entering the search buffer to those that match a path

--- settings.py
+++ settings.py
@@ -10,3 +10,4 @@ persistentMode = True
 lightningPathFile = os.sys.argv[1]
 showHiddenFiles = False
 hiddenFilesPattern = '(^\\..*)|(.*~$)|(.*\\.(swp|pyc|fasl|o|spec|bak)$)|(__pycache__?.)'
+restrictBuffer = True
