8750491e8b23df9a52db750b4c67c486a027ee12
fouric <fouric@protonmail.com>
2019-08-19 17:31:05 -0400

    move list-splitting code into separate SPLIT-LIST function; draw multiple columns of names

--- src/lightning-cd.lisp
+++ src/lightning-cd.lisp
@@ -3,15 +3,34 @@
 (defun append-char (string char)
   (concatenate 'string string (coerce (list char) 'string)))
 
-(defun draw (chunks buffer)
+(defun draw (columns buffer)
   (f:clear-window t)
-  (let ((y -1))
-    (dolist (item (reverse (first (last chunks))))
-      (f:write-string-at (hacky-name (namestring item)) 1 (incf y))))
+  (let ((x 1))
+    (dolist (column columns)
+      (let ((width (+ 3 (apply #'max (mapcar #'length (mapcar #'hacky-name column)))))
+            (y -1))
+        (dolist (item column)
+          (f:write-string-at (hacky-name (namestring item)) x (incf y)))
+        (incf x width))))
   (f:write-string-at (concatenate 'string "> " buffer) 1 (1- f:*screen-height*) f:+color-white-black+)
   #++(with-color +color-white-black+
        (charms:write-string-at-point *charms-win* (concatenate 'string ": " *shell-contents*) 1 (1- height)))
   (f:refresh-window))
+
+(defun split-list (list max-sublist-size)
+  "takes a single flat list and splits it into a list of sublists each with no more than MAX-SUBLIST-SIZE elements"
+  (let ((count 0)
+        sublists
+        chunk)
+    (dolist (item list)
+      (when (>= count max-sublist-size)
+        (push (nreverse chunk) sublists)
+        (setf count 0
+              chunk nil))
+      (push item chunk)
+      (incf count))
+    (push chunk sublists)
+    (nreverse sublists)))
 
 (defun hacky-name (namestring)
   ;; use PATHNAME-DIRECTORY
@@ -22,44 +41,37 @@
   (f:update-swank)
   (f:update-charms-dimensions)
   (unless current-directory
-    (setf current-directory (user-homedir-pathname)))
+    (setf current-directory (concatenate 'string (namestring (user-homedir-pathname)) "/")))
+  (setf current-directory (concatenate 'string (namestring (user-homedir-pathname)) "/"))
   (let ((char (f:get-char))
         dirty?)
     (when char
       (case char
         (#\esc
          (return-from main-loop))
-        (#\rubout
+        (#\rubout ;; backspace
          (setf buffer (subseq buffer 0 (max 0 (1- (length buffer))))
                dirty? t))
-        (#\page
+        (#\page ;; what?
          (fresh-line)
          (format t "cwd is ~s~%" current-directory))
         (t
-         (setf buffer (append-char buffer char)
-               dirty? t))))
+         (if (<= (char-code #\space) (char-code char) (char-code #\~))
+           (setf buffer (append-char buffer char)
+                 dirty? t)
+           (format t "got char ~s~%" char)))))
     ;;(setf current-directory "/home/fouric/async/")
     (when dirty?
+      ;; we should definitely not re-list directory contents in user interaction loop - use entr(1) or something in a background process, or only refresh when explicitly requested
       (let* ((directories (uiop:directory-files current-directory))
              (files (uiop:subdirectories current-directory))
-             (everything (append files directories))
+             (everything (sort (mapcar #'namestring (append files directories)) #'string<))
              (height (- f:*screen-height* 2))
-             (chunks nil)
-             (count 0)
-             chunk)
-        (dolist (item everything)
-          (when (>= count height)
-            (push chunk chunks)
-            (setf count 0
-                  chunk nil))
-          (push item chunk)
-          (incf count))
-        (push chunk chunks)
-        (setf chunks (nreverse chunks))
-        (draw chunks buffer)))
+             (columns (split-list everything height)))
+        (draw columns buffer)))
     (main-loop buffer current-directory)))
 
-(defun lightning-cd (&optional (current-directory (user-homedir-pathname)))
+(defun lightning-cd (&optional (current-directory (concatenate 'string (namestring (user-homedir-pathname)) "/")))
   (f:with-charms (:timeout 100 :color nil :raw-input t :interpret-control-characters t)
     (main-loop "" current-directory)))
 
