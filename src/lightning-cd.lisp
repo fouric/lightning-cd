214ac9d1fd4b2ada4ef6f891f2f56184a52266ae
fouric <fouric@protonmail.com>
2019-08-27 14:00:24 -0400

    remove some dead code; add filtering functions; restructure main loop control flow a bit

--- src/lightning-cd.lisp
+++ src/lightning-cd.lisp
@@ -1,6 +1,7 @@
 (in-package :lightning-cd)
 
 (defun hacky-name (namestring)
+  "take a NAMESTRING and heuristically return the actual name component after separating it from the path"
   ;; use PATHNAME-DIRECTORY
   (let ((chunks (remove-if-not (lambda (str) (plusp (length str))) (split-sequence:split-sequence #\/ namestring))))
     (first (last chunks))))
@@ -16,6 +17,7 @@
        (sort (mapcar (lambda (blob) (cons (car blob) (namestring (cdr blob)))) (append files directories)) #'string< :key #'cdr)))))
 
 (defun draw-item (item x y)
+  "take a single (type . pathspec) directory listing ITEM and render it at screen position (X, Y)"
   (case (car item)
     (:file
      (f:write-string-at (hacky-name (namestring (cdr item))) x y))
@@ -23,17 +25,36 @@
      (f:write-string-at (append-char (hacky-name (namestring (cdr item))) #\/) x y f:+color-blue-black+))))
 
 (defun draw-column (column x y)
+  "draw a COLUMN which is a list of cons cells of (type . pathspec) down from screen position (X, Y)"
   ;; each column is a list of CONS CELLS (car is :directory/:file, cdr is path) and not just paths
   (let ((width (+ 3 (apply #'max (mapcar #'length (mapcar #'hacky-name (mapcar #'cdr column))))))
         (y (1- y)))
     (dolist (item column width)
       (draw-item item x (incf y)))))
 
+(defun printable-character? (char)
+  "determine whether or not CHAR is a printable (ASCII) character"
+  ;; #\null is ascii d0 and #\Us is ascii d31
+  (not (or (<= (char-code #\null) (char-code char) (char-code #\Us))
+           ;; 127 is escape and 160 is the last unprintable ascii character
+           (<= 127 (char-code char) 160))))
+
+(defun filter-character (char)
+  (when (and (printable-character? char)
+             (not (member char '(#\comma #\space #\semicolon #\' #\" #\?))))
+    char))
+
+(defun filter-string (string)
+  (remove-if-not #'filter-character string))
+
 (defun draw (buffer current-directory)
+  "for a given filtering text BUFFER and pathspec CURRENT-DIRECTORY clear the terminal and draw the contents of said directory"
+  ;; only called when dirty so we can do whatever we want here
   (f:clear-window t)
   (let* ((everything (list-items current-directory))
+         (filtered (remove-if-not (lambda (term) (search (string-downcase buffer) (string-downcase term))) everything :key (lambda (item) (hacky-name (cdr item)))))
          (height (- f:*screen-height* 2))
-         (columns (split-list everything height))
+         (columns (split-list filtered height))
          (x 1))
     (dolist (column columns)
       (incf x (draw-column column x 0)))
@@ -45,28 +66,25 @@
   (f:update-charms-dimensions)
   (unless current-directory
     (setf current-directory (concatenate 'string (namestring (user-homedir-pathname)) "/")))
-  (setf current-directory (concatenate 'string (namestring (user-homedir-pathname)) "/"))
   (let ((char (f:get-char))
         dirty?)
     (when char
-      (case char
-        (#\esc
-         (return-from main-loop))
-        (#\rubout ;; backspace
-         (setf buffer (subseq buffer 0 (max 0 (1- (length buffer))))
-               dirty? t))
-        (#\page ;; what?
-         (fresh-line)
-         (format t "cwd is ~s~%" current-directory))
-        (#\Vt ;; C-k
-         (setf buffer ""
-               dirty? t))
-        (t
-         (if (<= (char-code #\space) (char-code char) (char-code #\~))
-           (setf buffer (append-char buffer char)
-                 dirty? t)
-           (format t "got char ~s~%" char)))))
-    ;;(setf current-directory "/home/fouric/async/")
+      (if (filter-character char)
+        (setf buffer (append-char buffer char)
+              dirty? t)
+        (case char
+          (#\esc
+           (return-from main-loop))
+          (#\rubout ;; backspace
+           (setf buffer (subseq buffer 0 (max 0 (1- (length buffer))))
+                 dirty? t))
+          (#\page ;; C-l
+           (format t "~&cwd is ~s~%" current-directory))
+          (#\Vt ;; C-k
+           (setf buffer ""
+                 dirty? t))
+          (t
+           (format t "~&got code ~s~%" char)))))
     (when dirty?
       ;; we should definitely not re-list directory contents in user interaction loop - use entr(1) or something in a background process, or only refresh when explicitly requested
       (draw buffer current-directory))
